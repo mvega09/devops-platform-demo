88f1563a269d9497113009d86c29f9b39c546ae6
mvega09 <matiusve09@outlook.com>
2026-02-16 18:55:19 -0500

    Se realiza integracion con la DB en el codigo main

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 import os
 
-APP_NAME = os.getenv("APP_NAME", "DevOps Platform Demo V1.2")
+APP_NAME = os.getenv("APP_NAME", "DevOps Platform Demo V1.3")
 APP_ENV = os.getenv("APP_ENV", "dev")
--- app/main.py
+++ app/main.py
@@ -2,6 +2,30 @@ from fastapi import FastAPI
 from prometheus_client import Counter, generate_latest
 from starlette.responses import Response
 import logging
+import os
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+# 1. Configuración de Base de Datos
+# La URL la construiremos con variables de entorno que inyectaremos vía Helm
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres123@localhost:5432/devops_db")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# 2. Modelo de Usuario
+class Usuario(Base):
+    __tablename__ = "usuarios"
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(50))
+    apellido = Column(String(50))
+    email = Column(String(100), unique=True, index=True)
+
+# Crear tablas automáticamente
+Base.metadata.create_all(bind=engine)
 
 from config import APP_NAME, APP_ENV
 
@@ -10,12 +34,14 @@ app = FastAPI(title=APP_NAME)
 # Logging básico (producción friendly)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+REQUEST_COUNT = Counter("app_requests_total", "Total de peticiones")
 
-# Métrica Prometheus
-REQUEST_COUNT = Counter(
-    "app_requests_total",
-    "Total de peticiones a la aplicación"
-)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/health")
 def health():
@@ -24,6 +50,21 @@ def health():
         "status": "ok",
         "environment": APP_ENV
     }
+
+@app.get("/usuarios")
+def obtener_usuarios(db: Session = Depends(get_db)):
+    """Lista todos los usuarios en la base de datos"""
+    REQUEST_COUNT.inc()
+    return db.query(Usuario).all()
+
+@app.post("/usuarios")
+def crear_usuario(nombre: str, apellido: str, email: str, db: Session = Depends(get_db)):
+    """Registra un nuevo usuario"""
+    db_user = Usuario(nombre=nombre, apellido=apellido, email=email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 @app.get("/items")
 def get_items():
